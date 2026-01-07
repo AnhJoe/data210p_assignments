bfe1878b66702f6407ed393c3b4f6ac44a3e895e
DrNekoSenpai <haesungbecker@gmail.com>
2026-01-06 22:23:36 -0800

    Revert "initial commit for my branch"
    This reverts commit fa3c0f4b21ff8d537d15b29da65374bab6393d5a.

--- HW1/Part1_Regression_Prostate/Regression_Prostate_Analysis.R
+++
@@ -1,8 +0,0 @@
-rm(list = ls())
-
-install.packages(c("tidyverse", "GGally", "broom", "lmtest", "car"))
-library(tidyverse)  # ggplot2, dplyr, readr, etc.
-library(GGally)     # ggpairs
-library(broom)      # tidy model outputs
-library(lmtest)     # bptest (Breusch–Pagan)
-library(car)        # influence plots (optional)
